f3e5408f231add5cfef03b85efa2660d1bec1770
Amancodes26 <amansngh8586@gmail.com>
2025-06-15 22:09:27 +0530

    Add AdminInfo model interface

---
+++ grievance-backend/src/models/AdminInfo.ts
@@ -0,0 +1,14 @@
+export interface AdminInfo{
+    id?: number;
+    name: string;
+    email: string;
+    phone?: string;
+    password?: string;
+    isverified?: boolean;
+    IsActive: boolean;
+    LastLogin?: Date;
+    createdat?: Date;
+    updatedat?: Date;
+    role?: 'ADMIN' | 'SUPER_ADMIN';
+    permissions?: string[]; // Array of permission strings
+}
