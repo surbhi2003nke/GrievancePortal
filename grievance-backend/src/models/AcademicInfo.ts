84fa1db6a3d80ea566fd9908f5b94ff8953495da
DevanshChhabra <devanshchhabra2020@gmail.com>
2025-06-15 22:02:18 +0530

    added academic model

---
+++ grievance-backend/src/models/AcademicInfo.ts
@@ -0,0 +1,20 @@
+export interface AcademicInfo {
+  Id?: number;
+  RollNo: string;
+  ProgramId: number;
+  AcademicYear: string;
+  Term: number;
+  CampusId: number;
+  Batch: number;
+  CreatedAt?: Date;
+  UpdatedAt?: Date;
+}
+
+export interface CreateAcademicInfoData {
+  RollNo: string;
+  ProgramId: number;
+  AcademicYear: string;
+  Term: number;
+  CampusId: number;
+  Batch: number;
+}
