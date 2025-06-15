1fdf26924bc7308a8e0f76a387a62d0488161c08
Amancodes26 <amansngh8586@gmail.com>
2025-06-15 22:11:51 +0530

    Add PersonalInfo model interface

---
+++ grievance-backend/src/models/PersonalInfo.ts
@@ -0,0 +1,20 @@
+export interface PersonalInfo {
+  id?: number;
+  rollno: string;
+  name: string;
+  father?: string;
+  mother?: string;
+  abc?: string;
+  dob?: Date;
+  category?: string;
+  gender?: string;
+  pwd?: boolean;
+  phone?: string;
+  email: string;
+  password?: string;
+  isverified?: boolean;
+  admissionyear?: number;
+  le?: boolean;
+  createdat?: Date;
+  updatedat?: Date;
+}
